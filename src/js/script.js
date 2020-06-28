f1dbf40a927f5c02c64d23f223f75bc5d5d8a89a
Ratatujef <ratatujef@gmail.com>
2020-06-28 21:41:23 +0300

    need optimization

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@ inputRUB.addEventListener("input", () => {
   let request = new XMLHttpRequest();
   // reguest.open(method, url, assing, login, password);
   request.open("GET", "js/current.json");
-  request.setRequestHeader("Content-type", "application/json", "charset=utf-8");
+  request.setRequestHeader("Content-type", "application/json, charset=utf-8");
   request.send();
 
   //status
@@ -17,6 +17,30 @@ inputRUB.addEventListener("input", () => {
   request.addEventListener("readystatechange", () => {
     if (request.status == 200 && request.readyState === 4) {
       let data = JSON.parse(request.response);
+      inputUSD.value = inputRUB.value / data.usd;
+    } else {
+      inputUSD.value = "ошибка";
+    }
+  });
+});
+
+inputUSD.addEventListener("input", () => {
+  let request = new XMLHttpRequest();
+  // reguest.open(method, url, assing, login, password);
+  request.open("GET", "js/current.json");
+  request.setRequestHeader("Content-type", "application/json, charset=utf-8");
+  request.send();
+
+  //status
+  //statusText
+  //responseText / response
+  //readyState
+  request.addEventListener("readystatechange", () => {
+    if (request.status == 200 && request.readyState === 4) {
+      let data = JSON.parse(request.response);
+      inputRUB.value = inputUSD.value * data.usd;
+    } else {
+      inputRUB.value = "ошибка";
     }
   });
 });
