281fa908d4304664e9b9713e7b0f5e4a45e3975c
ratatujef <ratatujef@gmail.com>
2020-06-29 12:27:33 +0300

    done

--- src/js/script.js
+++ src/js/script.js
@@ -23,18 +23,13 @@ inputRUB.addEventListener("input", () => {
     }
   });
 });
-
+// обратная конвертация
 inputUSD.addEventListener("input", () => {
   let request = new XMLHttpRequest();
-  // reguest.open(method, url, assing, login, password);
   request.open("GET", "js/current.json");
   request.setRequestHeader("Content-type", "application/json, charset=utf-8");
   request.send();
 
-  //status
-  //statusText
-  //responseText / response
-  //readyState
   request.addEventListener("readystatechange", () => {
     if (request.status == 200 && request.readyState === 4) {
       let data = JSON.parse(request.response);
